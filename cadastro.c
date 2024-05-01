0a2a9cee32d3647b0f478fc5ef8f3254f6ed7394
LuiisMarim <luisaugustomarimm@gmail.com>
2024-05-01 10:20:08 -0300

    document: adição do salvamento permanente
    no formato .txt os pacientes estão sendo cadastrados
    
    Mudanças a serem submetidas:
    	modified:   cadastro.c

--- cadastro.c
+++ cadastro.c
@@ -17,11 +17,10 @@ typedef struct Lista {
     Celula *primeiro;
 } Lista;
 
-
 void inserirLDE(Lista *l, char *nome, int idade, char *rg, int dia, int mes, int ano) {
     Celula *novo = malloc(sizeof(Celula));
     if (novo == NULL) {
-        printf("Vazio\n");
+        printf("Memória insuficiente.\n");
         return;
     }
     strcpy(novo->nome, nome);
@@ -56,7 +55,6 @@ void inserirLDE(Lista *l, char *nome, int idade, char *rg, int dia, int mes, int
     l->qtde++;
 }
 
-
 void removerLDE(Lista *l, char *rg) {
     Celula *atual = l->primeiro;
     Celula *anterior = NULL;
@@ -88,6 +86,10 @@ Lista *inicializa_LDE() {
 
 void imprimirLDE(Lista *l) {
     Celula *atual = l->primeiro;
+    if (atual == NULL) {
+        printf("Lista vazia.\n");
+        return;
+    }
     while (atual != NULL) {
         printf("Nome: %s, Idade: %d, RG: %s, Data de Nascimento: %d/%d/%d\n",
                atual->nome, atual->idade, atual->rg, atual->dia, atual->mes, atual->ano);
@@ -96,62 +98,80 @@ void imprimirLDE(Lista *l) {
 }
 
 int cadastroDePaciente(){
-  char nome[100];
-  char idade_str[10];
-  char dia_str[10];
-  char rg[100];
-  char mes_str[10];
-  char ano_str[10];
-  int idade, dia, mes, ano, escolhaUsuario;
-  Lista *l = inicializa_LDE();
+    char nome[100];
+  
+    char idade_str[10];
+  
+    char dia_str[10];
+  
+    char rg[100];
+  
+    char mes_str[10];
+  
+    char ano_str[10];
+  
+    int idade, dia, mes, ano, escolhaUsuario;
+  
+    Lista *l = inicializa_LDE();
+ 
+    FILE *pacientes;
+  
+    do {
+        printf("\nInsira os dados do paciente abaixo: \n");
+        printf("Nome: ");
+        fgets(nome, sizeof(nome), stdin);
+        nome[strcspn(nome, "\n")] = '\0'; 
+        printf("Idade: ");
+        fgets(idade_str, sizeof(idade_str), stdin);
+        idade = atoi(idade_str); 
+        printf("RG: ");
+        fgets(rg, sizeof(rg), stdin);
+        rg[strcspn(rg, "\n")] = '\0'; 
+        printf("Dia do nascimento: ");
+        fgets(dia_str, sizeof(dia_str), stdin);
+        dia = atoi(dia_str); 
+        printf("Mês do nascimento: ");
+        fgets(mes_str, sizeof(mes_str), stdin);
+        mes = atoi(mes_str); 
+        printf("Ano do nascimento: ");
+        fgets(ano_str, sizeof(ano_str), stdin);
+        ano = atoi(ano_str); 
+        if (dia > 31 || mes > 12 || ano < 1910) {
+          printf("\nDatas inválidas\n");
+        } else if (mes == 2) {
+            if (ano % 4 == 0) {
+                if (dia > 29) {
+                  printf("\nDatas inválidas\n");
+              }
+              } else {
+              if (dia > 28) {
+                  printf("\nDatas inválidas\n");
+              }
+          }
+        }else{
+        inserirLDE(l, nome, idade, rg, dia, mes, ano);
+        printf("\n");
+        printf("Pacientes cadastrados até o momento:\n\n");
+        imprimirLDE(l);
+        printf("\nContinuar cadastramento ?\n\n");
+        printf("1 - SIM // 2 - Salvar em Arquivo // 0 - Cancelar operação)\n");
+        printf("Escolha: ");
+        scanf("%d", &escolhaUsuario);
+        getchar(); 
+        if (escolhaUsuario == 2){
+          Celula *atual = l->primeiro;
+          while (atual != NULL){
+            pacientes = fopen("pacientes.txt", "a");
+            fprintf(pacientes, "Nome: %s, Idade: %d, RG: %s, Dia: %d, Mês: %d, Ano: %d \n", atual->nome, atual->idade, atual -> rg, atual-> dia, atual->mes, atual-> ano);
+            atual = atual->proximo;
+            printf("\nIndivíduo cadastrado no banco de pacientes.\n\n");
+          }
+          fclose(pacientes);
+          return 0;
+        } 
+      }
+    } while (escolhaUsuario != 0);
 
-  do{
-
-      
-      printf("Insira os dados do paciente abaixo: ");
-      printf("\n");
-      printf("\n");
-      getchar();
-      printf("Nome: ");
-      fgets(nome, sizeof(nome), stdin);
-      nome[strcspn(nome, "\n")] = '\0'; 
-      printf("\n");
-      printf("Idade: ");
-      fgets(idade_str, sizeof(idade_str), stdin);
-      idade = atoi(idade_str); 
-      printf("\n");
-      printf("RG: ");
-      fgets(rg, sizeof(rg), stdin);
-      rg[strcspn(rg, "\n")] = '\0'; 
-      printf("\n");
-      printf("Dia do nascimento: ");
-      fgets(dia_str, sizeof(dia_str), stdin);
-      dia = atoi(dia_str); 
-      printf("\n");
-      printf("Mês do nascimento: ");
-      fgets(mes_str, sizeof(mes_str), stdin);
-      mes = atoi(mes_str); 
-      printf("\n");
-      printf("Ano do nascimento: ");
-      fgets(ano_str, sizeof(ano_str), stdin);
-      ano = atoi(ano_str); 
-      printf("\n");
-
-      inserirLDE(l, nome, idade, rg, dia, mes, ano);
-
-      printf("Pacientes cadastrados até o momento: ");
-      printf("\n");
-      printf("\n");
-      imprimirLDE(l);
-      printf("\n");
-      printf("\n");
-      printf("Continuar cadastramento ? ");
-      printf("\n");
-      printf("1 - SIM // 2 - NÃO");  
-      printf("\n");
-      scanf("%d", &escolhaUsuario);
-      getchar();
-
-  } while (escolhaUsuario != 2); 
-  return 0;
+    return 0;
 }
+
